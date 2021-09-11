9ff71d79e0a81301a412b16f469fd8c15211e45f
Mattias Xu <mattias_xu@hotmail.com>
2021-09-11 16:31:45 +0200

    work

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import torch
 import os
+import PIL
 from torch.utils.data import Dataset
 
 class DrivingDataset(Dataset):
@@ -7,6 +8,18 @@ class DrivingDataset(Dataset):
         self.path = path
         self.frames = frames
         self.imgs = os.listdir(path)
+        self.transform = None
     
     def __len__(self):
-        return len(self.imgs) - self.frames + 1
+        return len(self.imgs)//8 - 1
+    
+    def __getitem__(self, idx):
+        video = torch.zeros(3, 16, 256, 256)
+        time = 0
+        for i in range(idx*8, idx*8+16):
+            img = PIL.Image.open(
+                self.path + f"/{idx*8}" + ".jpg"
+            )
+            img = self.transform(img)
+            video[:, time, :, :] = img 
+        return video
--- vidvqvae.py
+++ vidvqvae.py
@@ -118,6 +118,83 @@ class Decoder(nn.Module):
     def forward(self, input):
         return self.blocks(input)
 
+class VQVAE(nn.Module):
+    def __init__(
+        self,
+        in_channel=3,
+        channel=128,
+        n_res_block=2,
+        n_res_channel=32,
+        embed_dim=64,
+        n_embed=512,
+        decay=0.99,
+    ):
+        super().__init__()
+
+        self.enc_b = Encoder(in_channel, channel, n_res_block, n_res_channel, stride=4)
+        self.enc_t = Encoder(channel, channel, n_res_block, n_res_channel, stride=2)
+        self.quantize_conv_t = nn.Conv3d(channel, embed_dim, 1)
+        self.quantize_t = Quantizer(embed_dim, n_embed)
+        self.dec_t = Decoder(
+            embed_dim, embed_dim, channel, n_res_block, n_res_channel
+        )
+        self.quantize_conv_b = nn.Conv3d(channel, embed_dim, 1)
+        self.quantize_b = Quantizer(embed_dim, n_embed)
+        self.upsample_t = nn.ConvTranspose3d(
+            embed_dim, embed_dim, 4, stride=2, padding=1
+        )
+        self.dec = Decoder(
+            embed_dim + embed_dim,
+            in_channel,
+            channel,
+            n_res_block,
+            n_res_channel
+            # stride=4,
+        )
+
+    def forward(self, input):
+        quant_t, quant_b, diff, _, _ = self.encode(input)
+        dec = self.decode(quant_t, quant_b)
+
+        return dec, diff
+
+    def encode(self, input):
+        enc_b = self.enc_b(input)
+        enc_t = self.enc_t(enc_b)
+
+        quant_t = self.quantize_conv_t(enc_t).permute(0, 2, 3, 4, 1)
+        quant_t, diff_t, id_t = self.quantize_t(quant_t)
+        quant_t = quant_t.permute(0, 4, 1, 2, 3)
+        diff_t = diff_t.unsqueeze(0)
+
+        # dec_t = self.dec_t(quant_t)
+        # enc_b = torch.cat([dec_t, enc_b], 1)
+
+        print(enc_b.shape)
+        quant_b = self.quantize_conv_b(enc_b).permute(0, 2, 3, 4, 1)
+        quant_b, diff_b, id_b = self.quantize_b(quant_b)
+        quant_b = quant_b.permute(0, 4, 1, 2, 3)
+        diff_b = diff_b.unsqueeze(0)
+
+        return quant_t, quant_b, diff_t + diff_b, id_t, id_b
+
+    def decode(self, quant_t, quant_b):
+        upsample_t = self.upsample_t(quant_t)
+        quant = torch.cat([upsample_t, quant_b], 1)
+        dec = self.dec(quant)
+
+        return dec
+
+    def decode_latent(self, code_t, code_b):
+        quant_t = self.quantize_t.embed_code(code_t)
+        quant_t = quant_t.permute(0, 4, 1, 2, 3)
+        quant_b = self.quantize_b.embed_code(code_b)
+        quant_b = quant_b.permute(0, 4, 1, 2, 3)
+
+        dec = self.decode(quant_t, quant_b)
+
+        return dec
+
 if __name__ == "__main__":
     rand_input = torch.rand((8, 3, 16, 256, 256))
     bot_test = Encoder(3, 4, 8, 8, stride=4)
@@ -127,3 +204,6 @@ if __name__ == "__main__":
     decoder = Decoder(4, 3, 128, 5, 10)
     print(bot_out.shape)
     print(decoder(bot_out).shape)
+    print("VQVAE")
+    final = VQVAE()
+    print(final(rand_input)[0].shape)
